efc33c7f7300f87b91753b1a2b8241e179feddfb
whojinn <github@mail.whojinn.com>
2022-03-16 20:02:07 +0900

    first commit.

---
+++ src/QuoterExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2022 whojinn
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *  http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Whojinn\Quoter;
+
+use League\CommonMark\Extension\ConfigurableExtensionInterface;
+
+/**
+ * commonmarkの引用文のための拡張機能。
+ *
+ */
+class QuoterExtension implements ConfigurableExtensionInterface
+{
+}
